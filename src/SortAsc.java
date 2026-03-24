45715996bc52dc51d4fac71ed9d066c1821f02e2
akushwaha-altimetrik <akushwaha@altimetrik.com>
2026-03-24 16:51:44 +0530

    Implement sorting and printing of a list in SortAsc

--- src/SortAsc.java
+++ src/SortAsc.java
@@ -1,2 +1,11 @@
 class SortAsc{
+   public static void main(String[] args) {
+        List<Integer> list=Arrays.asList(2,1,7,4,22,11);
+        sort(list); 
+    }
+    public static void sort(List<Integer> list){
+        list.stream().sorted((e1,e2)->e1.compareTo(e2)).collect(Collectors.toList())
+        .stream().forEach(e->System.out.println(e));
+    }
 }
+
