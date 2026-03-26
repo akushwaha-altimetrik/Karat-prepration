288f53341e7b3f3349d96312801664f74f859788
akushwaha-altimetrik <akushwaha@altimetrik.com>
2026-03-26 20:47:34 +0530

    Update RotateKNode.java

--- src/RotateKNode.java
+++ src/RotateKNode.java
@@ -1,13 +1,12 @@
 class RotateKNode{
   public ListNode rotateRight(ListNode head, int k) {
-        if(head==null || head.next==null)
+        if(head==null || head.next==null || k<1 )
         return head;
         Map<Integer,ListNode> map=new HashMap<>();
         ListNode node=head;
         int i=1;
         while(node!=null){
             map.put(i,node);
-             System.out.println(i+"-"+node.val);
             node=node.next;
             i++;
         }
@@ -15,15 +14,12 @@ class RotateKNode{
         if(k>i){
             k=k%i;
         }
+        if(k==i || k==0)
+        return head;
         ListNode leftNode=map.get(i-k);
-        System.out.println(map.get(i-k).val);
          ListNode f=leftNode.next;
-          //System.out.println("f-"+f.next.val);
-         leftNode.next=null;  
-         ListNode s=map.get(1);
-         ListNode result=f.next=s;
-         // ListNode left= map.get(i-k+1).next=map.get(1); 
-          System.out.println(result.val);
-          return result;
-    }
-}
+         leftNode.next=null; 
+         map.get(i).next=map.get(1);
+          
+          return f;
+    }}
