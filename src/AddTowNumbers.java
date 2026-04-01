07c36cf18a4afd7a39fddb21d59dc03c5e470278
akushwaha-altimetrik <akushwaha@altimetrik.com>
2026-04-02 01:07:03 +0530

    Update AddTowNumbers.java

--- src/AddTowNumbers.java
+++ src/AddTowNumbers.java
@@ -1,9 +1,9 @@
 class AddTowNumbers{
    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-         
-            ListNode list=null;
+             ListNode result=new ListNode(); 
+            ListNode list=result;
             int rm=0;
-            ListNode result=null; 
+           
             while(l1!=null && l2!=null){
                 int v=l1.val+l2.val+rm;
                 l1=l1.next;
@@ -11,8 +11,8 @@ class AddTowNumbers{
                 ListNode n1=new ListNode();
                 
                 if(v>9){
-                    n1.val=0;
-                    rm=1;
+                    n1.val=v%10;
+                    rm=v/10;
                 }else{
                    n1.val=v;
                     rm=0;
@@ -20,13 +20,12 @@ class AddTowNumbers{
                 if(list==null){
                     
                     list=n1;
-                    result=list;
                   
                 }else{
                     list.next=n1;
                     
                 }
-                list=list.next;
+               list=list.next;
             }
             ListNode rl=null;
             if(l1==null && l2!=null){
@@ -40,16 +39,21 @@ class AddTowNumbers{
                 rl=rl.next;
                  ListNode n1=new ListNode();
                 if(v>9){
-                    n1.val=0;
-                    rm=1;
+                     n1.val=v%10;
+                    rm=v/10;
                 }else{
                     n1.val=v;
                     rm=0;
                 }
+                list.next=n1;
+                list=list.next;
+               
             }
+            if(rm>0)
+            list.next=new ListNode(rm);
            
         
-       return result;
+       return result.next;
         
     }
 }
